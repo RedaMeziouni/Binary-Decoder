811a2d90995c3b68c3de889901075f5ec8394b85
RedaMeziouni <meziounidb@gmail.com>
2022-11-07 17:04:01 +0100

    Grab the necessary Elements

---
+++ app.js
@@ -0,0 +1,6 @@
+const formEl = document.querySelector("form");
+const outputEl = document.querySelector("#output");
+const inputEl = document.querySelector("#input");
+const switchEl = document.querySelector(".switch");
+const title = document.querySelector(".title");
+const copy = document.querySelector(".copy");
