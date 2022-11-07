e2e5c4b0a97ca89f91d8c2929a24a9aa24c9e811
RedaMeziouni <meziounidb@gmail.com>
2022-11-07 17:10:22 +0100

    Create convert eventListener

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const inputEl = document.querySelector("#input");
 const switchEl = document.querySelector(".switch");
 const title = document.querySelector(".title");
 const copy = document.querySelector(".copy");
+
+// Sumit button event listener
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = e.target.input.value;
+  const inputType = e.target.input.getAttribute("data-type");
+
+  convert(inputType, inputValue);
+});
+
+function convert() {};
